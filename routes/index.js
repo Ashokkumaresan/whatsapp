949bfcf3f0d69c17893ddd7dd0cebf8fe30919d7
ashokkumaresan <ashok1989.stone@gmail.com>
2019-11-16 14:53:36 +0530

    proper structure

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.use(cors())
 router.use(bodyParser.json());
 router.use(cookieParser());
 router.use(session({secret: "hotshot"}));
-path.basename('/dist/state/');
+path.basename('/');
 /*Start Login service*/
 var url='mongodb+srv://ashokkumaresan:Godfather@321@cluster0-rzsic.mongodb.net/test?retryWrites=true&w=majority'
 router.get('/api/login',function(req,res){
